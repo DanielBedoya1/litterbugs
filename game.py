9c903ec0d2a7a7063be74afaf1ef28b6c9761f49
DanielBedoya1 <113715199+DanielBedoya1@users.noreply.github.com>
2023-05-17 13:57:47 -0400

    Update game.py

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ WIDTH = 800
 HEIGHT = 512
 score = 0
 game_over = False
+game_menu = True
 
 bg = "background"
 
@@ -19,66 +20,85 @@ paper.pos = 100, 100
 trash = Actor("trash")
 trash.pos = 100, 100
 
-def draw():
-    screen.blit(bg, (0,0))
-    jim.draw()
-    can.draw()
-    paper.draw()
-    trash.draw()
-    screen.draw.text("Score: " + str(score), color="black", topleft=(10,10), fontsize=50)
+def start_timer():
+    clock.schedule(time_up, 30.0)
 
+
+def draw():
     if game_over:
-        screen.fill("pink")
+        screen.fill("blue")
         screen.draw.text("Final Score: " + str(score), topleft = (10,10), fontsize=100)
+    elif game_menu:
+        screen.fill("black")
+        screen.draw.text("Press Space to Start" , topleft = (10,10), fontsize=100)
+    else:
+        screen.blit(bg, (0,0))
+        jim.draw()
+        can.draw()
+        paper.draw()
+        trash.draw()
+        screen.draw.text("Score: " + str(score), color="black", topleft=(10,10), fontsize=50)
 
 def place_can():
     can.x = randint(50, (WIDTH - 20))
-    can.y = randint(50, (HEIGHT -20))
+    can.y = randint(50, (HEIGHT - 20))
+
+
 def place_paper():
     paper.x = randint(20, (WIDTH - 20))
-    paper.y = randint(20, (HEIGHT -20))
+    paper.y = randint(20, (HEIGHT - 20))
+
+
 def place_trash():
     trash.x = randint(20, (WIDTH - 20))
-    trash.y = randint(20, (HEIGHT -20))
+    trash.y = randint(20, (HEIGHT - 20))
+
+
 def time_up():
     global game_over
     game_over = True
 
+
 def update():
+    global game_menu
     global score
 
-    if keyboard.left:
-        jim.x = jim.x - 3
-    elif keyboard.right:
-        jim.x = jim.x +3
-    elif keyboard.up:
-        jim.y = jim.y -3
-    elif keyboard.down:
-        jim.y = jim.y +3
+    if game_menu:
 
-    can_collected = jim.colliderect(can)
+        if keyboard.space:
+            game_menu = False
+            start_timer()
 
-    if can_collected:
-        score = score + 1
-        place_can()
+    else:
+        if keyboard.left:
+            jim.x = jim.x - 3
+        elif keyboard.right:
+            jim.x = jim.x + 3
+        elif keyboard.up:
+            jim.y = jim.y - 3
+        elif keyboard.down:
+            jim.y = jim.y + 3
 
-    paper_collected = jim.colliderect(paper)
+        can_collected = jim.colliderect(can)
 
-    if paper_collected:
-        score = score + 1
-        place_paper()
+        if can_collected:
+            score = score + 1
+            place_can()
 
-    trash_collected = jim.colliderect(trash)
+        paper_collected = jim.colliderect(paper)
 
-    if trash_collected:
-        score = score + 1
-        place_trash()
+        if paper_collected:
+            score = score + 1
+            place_paper()
+
+        trash_collected = jim.colliderect(trash)
+
+        if trash_collected:
+            score = score + 1
+            place_trash()
 
 
-clock.schedule(time_up, 30.0)
+
 place_can()
 place_paper()
 place_trash()
-
-
-
